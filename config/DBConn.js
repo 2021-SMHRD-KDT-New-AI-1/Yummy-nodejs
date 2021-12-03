ef78e0f6f5d9e2868c9826ad236649f5e4f9cfbe
조현중 <chj113024@gmail.com>
2021-12-03 20:15:44 +0900

    Update DBConn.js
    팀 서버 연동

--- config/DBConn.js
+++ config/DBConn.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 let conn = mysql.createConnection({
     host : "project-db-stu.ddns.net",
-    user : "team_g_1",
-    password : "smhrd1",
+    user : "dbdb",
+    password : "123456",
     port : 3307,
-    database : "campus_g_1_1012"
+    database : "dbdb"
 });
 
 conn.connect();
